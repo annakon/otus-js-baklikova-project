3a8ab25352422322847a1204acbc24a5b44f5d9f
denis <denis.baklykov@boxberry.ru>
2022-12-19 13:37:14 +0300

    RadioButton

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RadioButton from "./components/RadioButton";
+
+const App= () => {
+    return <>
+        <h1>Вакансии для начинающих разработчиков</h1>
+        <RadioButton></RadioButton>
+    </>;
+}
+
+export default App;
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
+import App from "./App"
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLDivElement
 );
-root.render(<h1>Hello, world!</h1>);
+root.render(<App/>);
