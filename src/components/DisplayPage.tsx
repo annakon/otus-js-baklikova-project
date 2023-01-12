b2e66d90e048cffa91cc70e286c1483dd22a65bc
denis <denis.baklykov@boxberry.ru>
2023-01-12 16:19:41 +0300

    отображение по центру

--- src/components/DisplayPage.tsx
+++ src/components/DisplayPage.tsx
@@ -9,8 +9,8 @@ export const DisplayPage: FC = () => {
     //const name = useSelector(selectName);
 
     return <>
-        <h1>Вакансии для начинающих разработчиков</h1>
-        <RadioButton></RadioButton>
+        <h1 className="h1-center">Вакансии для начинающих разработчиков</h1>
+        <div className="radio-center"><RadioButton></RadioButton></div>
         <Loading></Loading>
-    </>
+        </>
 }
