f6db4a69c395f4853df63c7c136a724a460b46e9
denis <denis.baklykov@boxberry.ru>
2023-01-19 18:36:30 +0300

    отображение вакансии

--- src/components/VacancyInfo.tsx
+++ src/components/VacancyInfo.tsx
@@ -1,12 +1,48 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect} from 'react';
 import {useParams} from "react-router-dom";
+import {store} from "../store/store";
+import {error, loading, success} from "../store/profile/actions";
+import {useDispatch, useSelector} from "react-redux";
+import {getData, getError, isLoading} from "../store/profile/selectors";
 
 export const VacancyInfo: FC = () => {
 
     const { id } = useParams();
+    const dispatch = useDispatch();
 
-    return <>
-        <h1>Вакансия</h1>
-        <div>{id}</div>
-    </>
+    const Loading = useSelector(isLoading);
+    const iserror = useSelector(getError);
+    const isdata = useSelector(getData);
+
+    function loadData() {
+        // put your code here
+        return async (dispatch: typeof  store.dispatch, getstate: typeof store.getState)=>
+        {
+            dispatch(loading());
+            fetch(`https://api.hh.ru/vacancies/${id}`)
+                .then((response) => response.json())
+                .then((data: any) => dispatch(success(data)))
+                .catch((err: Error) => dispatch(error(err)));
+        }
+    }
+
+    useEffect(() => {
+        dispatch(loadData() as never);
+    }, []);
+
+    if (Loading) {
+        return <p>Loading....</p>;
+    }
+    if (iserror) {
+        return <h1>{iserror.message}</h1>;
+    }
+    if (isdata) {
+        return <div className="card-vacancy">
+            <h1><a className="card-link" href={isdata.alternate_url}>{isdata.name}</a></h1>
+            <p>{isdata.description?.replace(/<\/?[^>]+(>|$)/g, "")}</p>
+        </div>
+
+         //return <pre>{JSON.stringify(isdata, null, 2)}</pre>;
+    }
+    return <></>
 }
