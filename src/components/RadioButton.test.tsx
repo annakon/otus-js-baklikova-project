8538ca1488df0007d1031bf02a05241d179ec870
denis <denis.baklykov@boxberry.ru>
2023-01-31 15:25:09 +0300

    фиксим тест с fetch

--- src/components/RadioButton.test.tsx
+++ src/components/RadioButton.test.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import {render, screen} from '@testing-library/react';
+import fetchMock from "jest-fetch-mock";
+
 
 import {RadioButton} from './RadioButton';
 import {store} from "../store/store";
 import { Provider } from "react-redux";
 
 describe("testing RadioButton component", () => {
+    beforeEach(() => { // if you have an existing `beforeEach` just add the following lines to it
+        fetchMock.mockIf(/^https?:\/\/example.com.*$/, "{}")
+    })
+
     test("should render 3 Button", () => {
         render(<Provider store={store}><RadioButton /></Provider>);
 
