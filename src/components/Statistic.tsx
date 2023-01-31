7ba17d231c9a63d90aed526a8ef6065a022f753a
denis <denis.baklykov@boxberry.ru>
2023-01-31 16:48:50 +0300

    табличка

--- src/components/Statistic.tsx
+++ src/components/Statistic.tsx
@@ -5,5 +5,23 @@ export const Statistic: FC = () => {
 
     return <>
         <Menu></Menu>
+        <div className="TableSize">
+            <table className="table table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">React</th>
+                    <th scope="col">Angular</th>
+                    <th scope="col">Vue</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </>
 }
