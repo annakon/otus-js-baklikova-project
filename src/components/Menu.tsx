7e57f3e2a43e814c273d86ece561df53ee4a7aa3
denis <denis.baklykov@boxberry.ru>
2023-01-31 16:15:56 +0300

    выравнивание меню

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,7 +7,7 @@ export const Menu: FC = () => {
         <h1 className="h1-center">Вакансии для начинающих разработчиков</h1>
         <div className="container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
+                <div className="container-fluid" id="menu">
                     <NavLink className="navbar-brand" to="/">Вакансии</NavLink>
                     <NavLink className="navbar-brand" to="/statistic">Статистика</NavLink>
                 </div>
